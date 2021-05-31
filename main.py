26389060349843991d82233a75d810ead1f13e3d
mortimerma <mortimerma@tencent.com>
2021-05-31 15:46:35 +0800

    feat: 完善功能

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import service
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    service.search_news('新冠')
+    # service.get_weibo_time_num_bar_html()
+    # service.get_weibo_attitude_num_pie_html()
+    print(list(service.get_weibo_news_by_month('2020-06')))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import Response
 from flask_cors import CORS
 import service
 import utils
@@ -26,6 +27,43 @@ def get_news():
         return common_rep(True, utils.format_news([news])[0])
     else:
         return common_rep(True, msg='没有找到相关新闻')
+
+
+@app.route('/graph/comments', methods=['GET'])
+def get_comment_graph():
+    id = request.args['news_id']
+    if not id:
+        return common_rep(False, msg='缺少参数')
+    return service.get_news_comments_graph_html(id)
+
+
+@app.route('/graph/wordcloud', methods=['GET'])
+def get_comment_word_cloud():
+    id = request.args['news_id']
+    if not id:
+        return common_rep(False, msg='缺少参数')
+    img = service.get_comments_word_cloud(id)
+
+    return Response(img, mimetype='image/jpeg')
+
+
+@app.route('/graph/keyword', methods=['GET'])
+def get_keyword_graph():
+    keyword = request.args['keyword']
+    if not keyword:
+        return common_rep(False, msg='缺少参数')
+    return service.get_keyword_news_graph_html(keyword)
+
+
+@app.route('/graph/attitude/covid', methods=['GET'])
+def get_attitude_graph():
+    month = request.args.get('month')
+    return service.get_weibo_attitude_num_pie_html(month)
+
+
+@app.route('/graph/num/covid', methods=['GET'])
+def get_num_graph():
+    return service.get_weibo_time_num_bar_html()
 
 
 if __name__ == '__main__':
